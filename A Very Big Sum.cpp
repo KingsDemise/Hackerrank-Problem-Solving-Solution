dcafb4cd62d097019d68ce5f28a5e6ce5f2e785c
KingsDemise <72397855+KingsDemise@users.noreply.github.com>
2020-10-05 19:52:07 +0530

    Add files via upload

---
+++ A Very Big Sum.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    long int a[1000],n=1,i=0,s=0;
+    scanf("%li",&n);
+    for(i=0;i<n;i++)
+        {
+        scanf("%li ",&a[i]);
+        s=s+a[i];
+        }
+    printf("%li",s);
+      
+    
+    return 0;
+}
